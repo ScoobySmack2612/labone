095221a843b1d75eea14fdcec3c57bd4d79b4706
Shreddy5150 <navarro.heron@gmail.com>
2017-01-06 20:31:32 -0800

    data type subtracter added

--- labonefinished.py
+++ labonefinished.py
@@ -1,34 +1,58 @@
 from time import sleep
 result = ''
-def adder():
-    global result 
+
+def combiner(operation):
+    global result
+    print "you've chosen to engage the %ser" % (operation)
+    sleep(1)
     in1 = input("enter your first value: ")
     in2 = input("enter your second value: ")
     if type(in1) == type(in2) and type(in1) and type(in2) == int:
-        result = "Result of adding your integers is: %s" % (in1 + in2)
+        if operation == "add":
+            result = "Result of adding your integers is: %s" % (in1 + in2)
+        else:
+            result = "Result of subtracting your integers is: %s" % (in1 - in2)
     elif type(in1) == type(in2) and type(in1) and type(in2) == float:
-        result = "Result of adding your floats is: %s" % (in1 +in2)
+        if operation == "add":
+            result = "Result of adding your floats is: %s" % (in1 + in2)
+        else:
+            result = "Result of subtracting your floats is: %s" % (in1 - in2)
     elif type(in1) == type(in2) and type(in1) and type(in2) == str:
-        result = "Result of adding your strings is: %s" % (in1 +in2)
+        if operation == "add":
+            result = "Result of adding your strings is: %s" % (in1 + in2)
+        else:
+            result = "Result of subtracting your strings is: %s" % (in1 - in2)
     elif type(in1) == type(in2) and type(in1) and type(in2) == tuple:
-        result = "Result of adding your tuples is: %s" % (in1 +in2)
+        if operation == "add":
+            result = "Result of adding your tuples is: %s" % (in1 + in2)
+        else:
+            result = "Result of subtracting your tuples is: %s" % (in1 - in2)
     elif type(in1) == type(in2) and type(in1) and type(in2) == list:
-        result = "Result of adding your lists is: %s" % (in1 +in2)
+        if operation == "add":
+            result = "Result of adding your lists is: %s" % (in1 + in2)
+        else:
+            result = "Result of subtracting your lists is: %s" % (in1 - in2)
     elif type(in1) == type(in2) and type(in1) and type(in2) == bool:
-        result = "Result of adding your boolean logic operators is: %s" % (in1+in2)
+        if operation == "add":
+            result = "Result of adding your boolean values is: %s" % (in1 + in2)
+        else:
+            result = "Result of subtracting your boolean values is: %s" % (in1 - in2)
     else:
         result = "ERROR: YOUR INPUTS NEED TO BE OF SAME TYPE. PLEASE TRY AGAIN WHEN NO LONGER INEBRIATED"
     return result
 
 repeat = True
+operator = "What oporator would you like to add or subtract?"
+again = "would you like to play again?(1 for yes, 0 for no)"
 while repeat == True:
-
-    adder()
+    print operator
+    answer = input("Please put your answer in quotations: ")
+    combiner(answer)
 
     print "Calculating..."
-    sleep (1)
+    sleep (.75)
     print result
-    again = "would you like to play again?(1 for yes, 0 for no)"
+
     print again
     answer = input()
     if answer == 1:
